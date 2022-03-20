cf4d6ea05fe1ff08d1a9692215bd9bca259afeb5
Daniel Oliveira <danieloliveira@lampp-it.com.br>
2022-03-20 02:45:37 -0300

    Add files repository

---
+++ CreciSP.Repository/Repositories/Room/RoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreciSP.Domain.Services.Room
+{
+    internal class RoomRepository
+    {
+    }
+}
