948167e55808df7da2dbf1eff6df98fffb84a13a
Bruno Silva Santos de Carvalho <bruno.santos@iatec.com>
2022-03-20 02:36:52 -0300

    up class of tables

---
+++ CreciSP.Domain/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using CreciSP.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreciSP.Domain.Models
+{
+    public class Room
+    {
+        public Room()
+        {
+            
+        }
+
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Floor { get; set; }
+        public string Capacity { get; set; }
+        public RoomTypeEnum Type { get; set; }
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
